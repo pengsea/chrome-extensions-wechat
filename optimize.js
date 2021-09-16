e6c23f95497ed3fafeeb486d6e03588010f8d4c5
pengsea <pengseasea@qq.com>
2021-09-16 16:49:54 +0800

    Update optimize.js

--- optimize.js
+++ optimize.js
@@ -1,7 +1,7 @@
 var css = '.rich_media_area_primary_inner { max-width: none !important; }' +
     ' .qr_code_pc_inner{display:none;} ' +
     ' section{margin:0 !important;line-height:1em !important;min-height:0 !important;} ' +
-    ' p{line-height: 1em !important;} ' +
+    ' p{line-height: normal !important;} ' +
     ' .rich_media_content p {min-height: 0;} ' +
     ' br{display:none}',
     head = document.getElementsByTagName('head')[0],
